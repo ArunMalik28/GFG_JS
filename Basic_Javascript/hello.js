648d32f17505454bc712374b0fa5f117e0a43ec4
ArunMalik28 <arun.malik0028@gmail.com>
2024-08-16 18:44:09 +0530

    Commit Basic JS first file

---
+++ Basic_Javascript/hello.js
@@ -0,0 +1,5 @@
+console.log("hello Javascript");
+
+console.log("Hi Arun\nToday I started to learn JS from GFG\nI won't move to another TECK STACK now I will focus on completing this");
+
+console.log("Lets do the hard thing");
