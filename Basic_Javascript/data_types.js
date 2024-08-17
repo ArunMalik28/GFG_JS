a1e2dff9cc4eebd8fbc8dbe380a92ee43f787347
ArunMalik28 <arun.malik0028@gmail.com>
2024-08-17 20:23:50 +0530

    Day 02 of learning to become something

---
+++ Basic_Javascript/data_types.js
@@ -0,0 +1,26 @@
+//Primitve and non-primitive data-types
+//Primitive are those data types where a single value is stored to a variable.
+// Primitive --> String, Number, boolean, undefined, null
+
+// String
+let userName = "Arun Malik";
+console.log(userName);
+console.log(typeof userName);
+
+//Number
+let userAge = 25;
+console.log(userAge);
+console.log(typeof userAge);
+
+//Boolean
+let isValidUser = true;
+console.log(typeof isValidUser);
+
+//undefined
+let userDegree;
+console.log(typeof userDegree);
+
+//null
+let userAddress = null;
+console.log(userAddress);
+
---
+++ Basic_Javascript/variable.js
@@ -0,0 +1,13 @@
+var message = "Hi Arun";
+var message = "Hello Arun";
+var message = "Today is Saturday!!!!";
+
+console.log(message);
+
+let userName = "Arun Malik";
+
+userName = "Gourav";
+
+const admissionYear = 2018;
+
+//admissionYear = 2019;
